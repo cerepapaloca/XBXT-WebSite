901b053510f193d331351ff587cc6fb5d38f4aac
Ceres <cagutierrezpayares@gmail.com>
2025-02-27 14:04:38 -0500

    fix url

--- javascript/mapArt.js
+++ javascript/mapArt.js
@@ -1,11 +1,11 @@
 
 async function ImageGallery() {
-    let response = await fetch("https://localhost:8443/mapArtData/");
+    let response = await fetch("https://xbxt.xyz:8443/mapArtData/");
     let json = await response.json();
     const container = document.getElementById("mapContainer");
     for (const data of json) {
-        const index = `https://localhost:8443/mapArtImg/${data}`
-        let response = await fetch(`https://localhost:8443/mapArtData/${data}`);
+        const index = `https://xbxt.xyz:8443/mapArtImg/${data}`
+        let response = await fetch(`https://xbxt.xyz:8443/mapArtData/${data}`);
         let json = await response.json();
         const rowElement = document.createElement("a");
         rowElement.className = "grid-item inbox refImg"
