7ab74d2e3737d436fdeaccd35ae9c9c7332851e9
Ceres <122226217+cerepapaloca@users.noreply.github.com>
2025-07-15 20:00:23 -0500

    fix 8

--- src/pages/MapArt.jsx
+++ src/pages/MapArt.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Isotope from "isotope-layout";
 import '../css/mapArt.css';
-
+import background from "../assets/background-MapArtGallery.webp";
 
 export default function MapGallery() {
     // const updateFade = function () {
@@ -56,6 +56,7 @@ export default function MapGallery() {
                 console.error("Error al cargar imágenes:", err);
             }
         };
+        document.body.style.backgroundImage = `url(${background})`;
         fetchImages();
     }, []);
 
