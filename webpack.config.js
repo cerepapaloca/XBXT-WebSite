ff84ee4866310b7adec49ceed110c5ed2a6e8ccc
Ceres <122226217+cerepapaloca@users.noreply.github.com>
2025-07-15 20:47:38 -0500

    fix 14

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Ajusta esto según tu punto de entrada principal
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|jpeg|png|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: 'images/',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
